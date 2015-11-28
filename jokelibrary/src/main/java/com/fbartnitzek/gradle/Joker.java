ac50f29206d1e43b34bba3cd06c789212b340f15
fbartnitzek <frank_bartnitzek@web.de>
2015-11-28 12:40:30 +0100

    step 1: created jokelibrary provides 1 joke and it is toasted in app (a bit to short)

---
+++ jokelibrary/src/main/java/com/fbartnitzek/gradle/Joker.java
@@ -0,0 +1,12 @@
+package com.fbartnitzek.gradle;
+
+public class Joker {
+
+    public static String getJoke(){
+        //src; http://www.laughfactory.com/jokes/technology-jokes
+        return "Do not be racist; be like Mario. He's an Italian plumber, who was made by the " +
+                "Japanese, speaks English, looks like a Mexican, jumps like a black man, " +
+                "and grabs coins like a Jew!";
+    }
+
+}
