f876f6a29e445036c6f37025e8bb46dc943dc5f8
fbartnitzek <frank_bartnitzek@web.de>
2015-11-28 12:19:24 +0100

    changed package name

--- app/src/androidTest/java/com/fbartnitzek/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/fbartnitzek/gradle/builditbigger/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.udacity.gradle.builditbigger;
+package com.fbartnitzek.gradle.builditbigger;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/fbartnitzek/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/fbartnitzek/gradle/builditbigger/MainActivity.java
@@ -1,4 +1,4 @@
-package com.udacity.gradle.builditbigger;
+package com.fbartnitzek.gradle.builditbigger;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
